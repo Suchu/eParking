5206da7b2366b3a7e25f9138845e992089e7f3fd
Suchu <sulochana.bhujel92@gmail.com>
2014-09-23 17:40:30 +0545

    eparking setup

---
+++ index.php
@@ -0,0 +1,43 @@
+<html>
+<header>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	
+	<title>e-Parking</title>
+
+	
+</header>
+<body>
+
+	<div id="wrapper">
+		<div id="logo">
+		</div>
+		
+
+		<div id="banner">
+			<div id="menu">
+		<ul>
+			<a href="home"><li>HOME</li></a>
+			<a href="about"><li>ABOUT</li></a>
+			<a href="trafficrules"><li>TRAFFIC RULES</li></a>
+			<a href="services"><li>SERVICES</li></a>
+			<a href="contacts"><li>CONTACTS</li></a>
+		</ul>
+	</div>
+			
+	</div>
+	<div class="left-sidebar">
+		<form action="">
+ <fieldset>
+  <legend>Search:</legend>
+  Vehicle Type: <input type="text"><br>
+  Current City: <input type="text"><br>
+  <input type="submit" value="Search">
+  
+ </fieldset>
+</form>
+	</div>
+		
+		
+	</div>
+</body>
+</html>
